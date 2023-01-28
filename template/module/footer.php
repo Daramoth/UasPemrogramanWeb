2900974c72b86d6cfe09c65db43f921438f45993
daramoth <123800468+Daramoth@users.noreply.github.com>
2023-01-28 15:20:49 +0700

    Add files via upload

---
+++ template/module/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Trusted</b> Hotel
+        </div>
+        <strong>Copyright &copy; 2021 <a href="#">JAJA HOTEL</a>.</strong> All rights reserved.
+      </footer>
